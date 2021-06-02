a4a30a0f0e07814cc96d18e249bcf02442640737
Denys55 <denischerep55@gmail.com>
2021-06-02 21:10:05 +0300

    Черновой набросок
    Сделал черновой набросок обьекта User. Поля: чатАйди, почта, группа,  никнейм(если будет возможность спарсить у телеги)

---
+++ src/main/java/com/goit/telegrambot/User.java
@@ -0,0 +1,54 @@
+package com.goit.telegrambot;
+
+import java.util.Objects;
+
+public class User {
+
+    //id диалога
+    private long chatId;
+    //если можно будет передать по api
+    private String nickname;
+
+    private String email;
+    private String groupName;
+
+    public User(long chatId, String email, String groupName) {
+        this.chatId = chatId;
+        this.email = email;
+        this.groupName = groupName;
+    }
+
+    public long getChatId() {
+        return chatId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getGroupName() {
+        return groupName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return chatId == user.chatId && Objects.equals(email, user.email) && Objects.equals(groupName, user.groupName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(chatId, email, groupName);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "chatId=" + chatId +
+                ", email='" + email + '\'' +
+                ", groupName='" + groupName + '\'' +
+                '}';
+    }
+}
