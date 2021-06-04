b69cb86c593a6df7fec5cd6aba91fb36157e7314
oBielov <i.byelov@gmail.com>
2021-06-04 13:41:12 +0300

    Starting to build proof of concept model.

--- src/main/java/com/goit/telegrambot/Main.java
+++ src/main/java/com/goit/telegrambot/Main.java
@@ -1,9 +1,20 @@
 package com.goit.telegrambot;
 
+import com.google.api.services.sheets.v4.Sheets;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws TelegramApiException, GeneralSecurityException, IOException {
         UserNotificationTimer.run(20, 33);
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+        telegramBotsApi.registerBot(new TelegramApiController());
+        Sheets googleService = GoogleApiConfig.service();
     }
 
 }
