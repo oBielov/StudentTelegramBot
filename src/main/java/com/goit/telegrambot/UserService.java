52a92b9c438e666c8796df59268473d2c8a1913e
shanse <shansea.s@gmail.com>
2021-06-05 13:37:04 +0300

    add emailCheck

--- src/main/java/com/goit/telegrambot/UserService.java
+++ src/main/java/com/goit/telegrambot/UserService.java
@@ -1,5 +1,11 @@
 package com.goit.telegrambot;
 
+import org.apache.commons.validator.routines.EmailValidator;
+
 public class UserService {
+
+    public boolean emailCheck(String email){
+        return (EmailValidator.getInstance().isValid(email));
+    }
 
 }
