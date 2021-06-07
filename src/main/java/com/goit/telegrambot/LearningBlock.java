84d84b752c5b26ca595ab4d22bcc83af73b3cce9
oBielov <i.byelov@gmail.com>
2021-06-07 10:19:50 +0300

    Learning block.

--- src/main/java/com/goit/telegrambot/LearningBlock.java
+++ src/main/java/com/goit/telegrambot/LearningBlock.java
@@ -1,25 +1,20 @@
 package com.goit.telegrambot;
 
-import com.google.api.services.sheets.v4.Sheets;
 import com.google.api.services.sheets.v4.model.ValueRange;
 import lombok.Data;
 import lombok.SneakyThrows;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Data
 public class LearningBlock {
 
     private String groupId;
-    private int question;
-    List<Question> questions;
-    private static Sheets googleService = getService();
+    private int currentQuestion;
+    private List<Question> questions = new ArrayList<>();
     private static String sheetId = getSheetId();
 
-    @SneakyThrows
-    private static Sheets getService(){
-        return GoogleApiConfig.service();
-    }
 
     @SneakyThrows
     private static String getSheetId(){
@@ -28,10 +23,26 @@ public class LearningBlock {
 
     @SneakyThrows
     public List<List<Object>> receiveQuestions(){
-        ValueRange response = googleService.spreadsheets().values()
-                .get(sheetId, groupId)
+        ValueRange response = GoogleApiConfig.service().spreadsheets().values()
+                .get(sheetId, "JavaScrip!A4:C88")
                 .execute();
-
+        return response.getValues();
     }
 
+    public void fillQuestions(){
+        List<List<Object>> values = receiveQuestions();
+        for (List row : values){
+            if(row.isEmpty()){
+                continue;
+            }
+            Question question = new Question();
+            question.setQuestion(row.get(0).toString());
+            question.setAnswer(row.get(1).toString());
+            question.setUrl(row.get(2).toString());
+            questions.add(question);
+        }
+    }
+    public Question getQuestion(int questionNum){
+        return questions.get(questionNum);
+    }
 }
