46ba86385906a5f08dd27f9f31fa2384995ab35d
oBielov <i.byelov@gmail.com>
2021-06-08 12:53:53 +0300

    Testing User creation and some methods

--- src/main/java/com/goit/telegrambot/Main.java
+++ src/main/java/com/goit/telegrambot/Main.java
@@ -1,6 +1,7 @@
 package com.goit.telegrambot;
 
 import com.google.api.services.sheets.v4.Sheets;
+import lombok.extern.slf4j.Slf4j;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
@@ -8,13 +9,19 @@ import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 import java.io.IOException;
 import java.security.GeneralSecurityException;
 
+@Slf4j
 public class Main {
 
-    public static void main(String[] args) throws TelegramApiException, GeneralSecurityException, IOException {
-        UserNotificationTimer.run(20, 33);
+    public static void main(String[] args) throws TelegramApiException{
+        UserNotificationTimer.run(20, 0);
         TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
         telegramBotsApi.registerBot(new TelegramApiController());
-        Sheets googleService = GoogleApiConfig.service();
+
+        User user = new User(2123213231L);
+        user.setLearningBlockId("JavaScrip");
+        user.addQuestions();
+        Question question = user.getCurrentQuestion();
+        System.out.println(question);
     }
 
 }
--- src/main/java/com/goit/telegrambot/User.java
+++ src/main/java/com/goit/telegrambot/User.java
@@ -17,17 +17,31 @@ public class User {
     private String email;
     private String groupName;
     private int currentQuestion;
+    private LearningBlock currentBlock;
 
     public User(long chatId){
         this.chatId = chatId;
         this.email = "";
         this.groupName = "";
         this.currentQuestion = 0;
+        this.currentBlock = new LearningBlock();
     }
 
     public User(long chatId, String email, String groupName) {
         this.chatId = chatId;
         this.email = email;
         this.groupName = groupName;
+    }
+
+    public void setLearningBlockId(String groupId){
+        currentBlock.setGroupId(groupId);
+    }
+
+    public void addQuestions(){
+        currentBlock.fillQuestions();
+    }
+
+    public Question getCurrentQuestion(){
+        return currentBlock.getQuestion(currentQuestion);
     }
 }
