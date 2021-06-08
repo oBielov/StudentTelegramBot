b097b952d5346a39933f7ac7b223759ff4399304
shanse <shansea.s@gmail.com>
2021-06-08 10:30:43 +0300

    add Current question to User

--- src/main/java/com/goit/telegrambot/User.java
+++ src/main/java/com/goit/telegrambot/User.java
@@ -1,33 +1,25 @@
 package com.goit.telegrambot;
 
 import lombok.Data;
-import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 
-import java.util.Objects;
 @Data
 @RequiredArgsConstructor
 public class User {
 
     //id диалога
-    //@NonNull
     private Long chatId;
-    private String nickname;
 
     private String email;
     private String groupName;
-    private int currentQuestion;
+    private LearningBlock currentQuestion;
 
     public User(long chatId){
         this.chatId = chatId;
         this.email = "";
         this.groupName = "";
-        this.currentQuestion = 0;
-    }
-
-    public User(long chatId, String email, String groupName) {
-        this.chatId = chatId;
-        this.email = email;
-        this.groupName = groupName;
+        this.currentQuestion.setGroupId("");
+        this.currentQuestion.setCurrentQuestion(0);
+        this.currentQuestion.getQuestion(0);
     }
 }
