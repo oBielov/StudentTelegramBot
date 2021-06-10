3e7d4eb395d8a1b3ba28435c163d634bd74e9686
Riba33 <riba331845@gmail.com>
2021-06-10 21:52:04 +0300

    add getUsers() to UserList.

--- src/main/java/com/goit/user/UserList.java
+++ src/main/java/com/goit/user/UserList.java
@@ -6,6 +6,8 @@ import java.util.Map;
 public class UserList {
     private static Map<Long,User> users = new HashMap<>();
 
+    public static Map<Long,User> getUsers() { return users;}
+
     public static void newUser(long chatId){
         users.put(chatId, new User(chatId));
     }
