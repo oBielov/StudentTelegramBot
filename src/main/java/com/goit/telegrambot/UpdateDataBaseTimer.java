4fc9601dbec0c4171d6a63e16788c341328ea770
Riba33 <riba331845@gmail.com>
2021-06-11 17:17:44 +0300

    Добавлено обновление учебного блока всех пользователей.

--- src/main/java/com/goit/telegrambot/UpdateDataBaseTimer.java
+++ src/main/java/com/goit/telegrambot/UpdateDataBaseTimer.java
@@ -1,5 +1,6 @@
 package com.goit.telegrambot;
 
+import com.goit.user.UserList;
 import lombok.SneakyThrows;
 
 import java.util.Calendar;
@@ -10,8 +11,8 @@ public class UpdateDataBaseTimer {
         new RunUpdate().start();
     }
     static class RunUpdate extends Thread{
-        int hours = 4; // Set hours to upgrade.
-        int minutes = 3; // Set minutes to upgrade..
+        int hours = 0; // Set hours to upgrade.
+        int minutes = 0; // Set minutes to upgrade..
         @SneakyThrows
         @Override
         public void run() {
@@ -19,7 +20,7 @@ public class UpdateDataBaseTimer {
             while (wl == 0) {
                 Calendar calendar = new GregorianCalendar();
                 if (calendar.get(Calendar.HOUR) == hours && calendar.get(Calendar.MINUTE) == minutes){
-                    UserList.getUsers().forEach((k,v) -> v.getLearningBlock().fillQuestions());
+                    UserList.getUsers().forEach((k, v) -> v.getLearningBlock().fillQuestions());
                 }
                 Thread.sleep(60000);
             }
