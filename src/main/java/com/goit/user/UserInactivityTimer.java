a90db9a12ddab8b63b7cde2d75981abef61393d4
Vadim-Havrylitse <vfgavrilice@gmail.com>
2021-06-11 21:51:21 +0300

    Tied up Messages and NotificationTimer and InactivityTimer

--- src/main/java/com/goit/user/UserInactivityTimer.java
+++ src/main/java/com/goit/user/UserInactivityTimer.java
@@ -3,6 +3,7 @@ package com.goit.user;
 import com.goit.api.TelegramApiController;
 import com.goit.buttons.SendButton;
 import com.goit.buttons.SendText;
+import com.goit.messages.Messages;
 
 import java.util.*;
 
@@ -29,17 +30,17 @@ public class UserInactivityTimer {
                 listButtons.add("Нет");
                 sendButton.sendButton(chatId,"Продолжаем?", listButtons);
             }
-        }, 3*1000, 20*60*1000);//нужно поменять на время в задании 20 мин.
+        },20*60*1000, 20*60*1000);//нужно поменять на время в задании 20 мин.
     }
     //реакция на кнопку таймера неактивности "НЕТ"
     public static void stopUserCheckInactivity(Long chatId) {
         timerMap.get(chatId).cancel();
-        sendText.sendText(chatId, "Жаль!\nКогда будешь готов жми кнопку <b>ДАЛЕЕ</b> вверху!");
+        sendText.sendText(chatId, Messages.stopCheckInactivity());
     }
 
     //реакция на кнопку таймера неактивности "ДА"
     public static void continueUserCheckInactivity(Long chatId) {
-        sendText.sendText(chatId, "Супер!\nТогда продолжаем, жми кнопку <b>ДАЛЕЕ</b> вверху!");
+        sendText.sendText(chatId, Messages.continueCheckInactivity());
         updateUserCheckInactivity(chatId);
     }
 }
